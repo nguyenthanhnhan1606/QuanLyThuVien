b1a763fe6c3be1c5f105c5b4bb50ea3a3c633eaa
2051050314nhan <2051050314nhan@ou.edu.vn>
2023-03-28 11:19:27 +0700

    update

--- QuanLyThuVien/src/main/java/com/ktpm/quanlythuvien/DangKyController.java
+++ QuanLyThuVien/src/main/java/com/ktpm/quanlythuvien/DangKyController.java
@@ -16,6 +16,7 @@ import java.net.URL;
 import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
 import java.sql.Date;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -24,11 +25,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import static javafx.scene.control.ButtonType.OK;
+import static javafx.scene.control.ButtonType.YES;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.HBox;
 
 /**
  *
@@ -55,19 +60,19 @@ public class DangKyController implements Initializable {
     @FXML
     private ComboBox<DoiTuong> cbDoituong;
     @FXML
-    private ComboBox cbGioiTinh;
-    @FXML
     private DatePicker ngaysinh;
     @FXML
     private RadioButton rd1;
     @FXML
     private RadioButton rd2;
     ToggleGroup group1;
-    
+   
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         group1 = new ToggleGroup();
+        rd1.setUserData("Nam");
         rd1.setToggleGroup(group1);
+        rd2.setUserData("Nữ");
         rd2.setToggleGroup(group1);
         loadBP();
         loadDT();
@@ -97,12 +102,13 @@ public class DangKyController implements Initializable {
          
         Date date = Date.valueOf(this.ngaysinh.getValue());
         if (this.password.getText().equals(this.cfpassword.getText())) {
-            User u = new User(this.username.getText(), this.password.getText(), this.name.getText(),"nam",date, this.email.getText(), this.diachi.getText(), this.sdt.getText(), this.cbBoPhan.getSelectionModel().getSelectedItem().getMaBP(), this.cbDoituong.getSelectionModel().getSelectedItem().getMaDT());
+            User u = new User(this.username.getText(), this.password.getText(), this.name.getText(),group1.getSelectedToggle().getUserData().toString(),date, this.email.getText(), this.diachi.getText(), this.sdt.getText(), this.cbBoPhan.getSelectionModel().getSelectedItem().getMaBP(), this.cbDoituong.getSelectionModel().getSelectedItem().getMaDT());
             UserService user = new UserService();
             try {
                 if (user.addUser(u)) {
-                    MessageBox.getBox("Question", "Add question successful", Alert.AlertType.INFORMATION).show();
-                    App.setRoot("DangNhap");
+                    clearForm();
+                    MessageBox.getBox("Thông báo", "Bạn đã đăng ký tài khoản thành công", Alert.AlertType.INFORMATION).show();
+
                 }
             } catch (SQLException ex) {
                 MessageBox.getBox("Question", "Add question failed", Alert.AlertType.ERROR).show();
@@ -111,5 +117,31 @@ public class DangKyController implements Initializable {
         }
 
     }
-
+    
+    public void thoat(ActionEvent evt) throws IOException
+    {  
+       Optional<ButtonType> result= MessageBox.getBox1("Question", "Add question failed", Alert.AlertType.INFORMATION).showAndWait();
+       if (result.get()==ButtonType.YES)
+       {
+           App.setRoot("DangNhap");
+       } else {
+                   
+        }        
+ 
+    }
+    
+    public void clearForm()
+    {
+        this.username.clear();
+        this.password.clear();
+        this.cfpassword.clear();
+        this.email.clear();
+        cbBoPhan.getSelectionModel().selectFirst();
+        cbDoituong.getSelectionModel().selectFirst();
+        this.group1.setUserData(null);
+        this.name.clear();
+        this.ngaysinh.setValue(null);
+        this.sdt.clear();
+        this.diachi.clear();
+    }
 }
--- QuanLyThuVien/src/main/java/com/ktpm/utils/MessageBox.java
+++ QuanLyThuVien/src/main/java/com/ktpm/utils/MessageBox.java
@@ -4,7 +4,12 @@
  */
 package com.ktpm.utils;
 
+import com.ktpm.quanlythuvien.App;
+import java.util.Optional;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import static javafx.scene.control.ButtonType.YES;
 
 /**
  *
@@ -19,6 +24,16 @@ public class MessageBox {
         Alert a = new Alert(type);
         a.setTitle(title);
         a.setContentText(content);
+        return a;
+    }
+     public static Alert getBox1(String title, String content, Alert.AlertType type) {
+        Alert a = new Alert(type);
+        a.setTitle(title);
+        a.setContentText(content);
+        ButtonType bty= new ButtonType("YES",ButtonBar.ButtonData.YES);
+        ButtonType btn= new ButtonType("No",ButtonBar.ButtonData.NO);
+        ButtonType btc= new ButtonType("CANCEL",ButtonBar.ButtonData.CANCEL_CLOSE);
+        a.getButtonTypes().setAll(bty,btn,btc);
         
         return a;
     }
